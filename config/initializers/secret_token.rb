e6cf7863f467e2259155282bdb609fc34a4fdf45
Luciano Tavares <lucianomt@gmail.com>
2011-02-25 08:08:10 -0800

    Installed Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddit::Application.config.secret_token = '9bf9028349adca143a1752fcc9cb0f651e4fbf19842ced49408e349505dcf453fda14aa054f64a279a13330d6e77fd2e6d0207d661b67fe532ceaed24fbdf400'
