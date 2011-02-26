191a322c174c240ca41ba7b157d898f6d361731d
Luciano Tavares <lucianomt@gmail.com>
2011-02-26 12:14:59 -0800

    Added admin to application view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :complete_username
+  
+  def complete_username
+    # current_user = User.find_by_email(session[:email])
+    @username = "Anderson Silva"
+  end
+  
 end
