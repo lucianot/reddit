f8b5b147df6783c3815cba6401ec4695cc2dc20d
Luciano Tavares <lucianomt@gmail.com>
2011-02-26 08:10:21 -0800

    Added username to Devise login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :login
+  attr_accessor :login
+  
+  protected
+
+  def self.find_for_database_authentication(conditions)
+    login = conditions.delete(:login)
+    where(conditions).where(["username = :value OR email = :value", { :value => login }]).first
+  end
+  
 end
