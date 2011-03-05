25612a9c217ee604032e76162420802201b98440
Luciano Tavares <lucianomt@gmail.com>
2011-03-04 20:51:46 -0800

    Update name

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,42 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+User.delete_all
+Link.delete_all
+Vote.delete_all
+
 users = User.create([{:email => 'adminguy@email.com',
                       :password => 'letmein',
                       :password_confirmation => 'letmein',
-                      :username => 'adminguy'},
-                    { :email => 'randomuser@email.com',
+                      :username => 'adminguy',
+                      :admin => true },
+                    { :email => 'randomgirl@email.com',
                       :password => 'letmein',
                       :password_confirmation => 'letmein',
-                      :username => 'randomuser'}])
+                      :username => 'randomgirl' },
+                    { :email => 'randomguy@email.com',
+                      :password => 'letmein',
+                      :password_confirmation => 'letmein',
+                      :username => 'randomguy' }])
                       
-links = Link.create([{:title => 'This guy copied me!',
+links = Link.create([{:title => 'These guys copied my website! Should I sue?',
                       :url => 'http://www.reddit.com',
-                      :user_id => users.first},
-                    { :title => 'Has anybody tried this new language?',
-                      :url => 'http://tryruby.org/',
-                      :user_id => users[1]}])
+                      :user_id => users.first.id},
+                    { :title => 'I love Bacon!',
+                      :url => 'http://en.wikipedia.org/wiki/Francis_Bacon',
+                      :user_id => users[1].id },
+                    { :title => 'Anyone else know this famous Rails programmer?',
+                      :url => 'http://www.facebook.com',
+                      :user_id => users[2].id }])
                       
 Vote.create([{:user => users.first, :link => links.first, :value => 1},
             {:user => users.first, :link => links[1], :value => -1},
-            {:user => users[1], :link => links.first, :value => 1}])
-                      
+            {:user => users[1], :link => links.first, :value => 1},
+            {:user => users[1], :link => links[1], :value => 1},
+            {:user => users[2], :link => links.first, :value => -1},
+            {:user => users[2], :link => links[2], :value => 1},
+            {:user => users[1], :link => links[2], :value => 1},
+            {:user => users[0], :link => links[2], :value => 1}])
+            
+
+puts "Success!!"                 
