4bd025ece8220f707b839083974f47ef552ceade
Luciano Tavares <lucianomt@gmail.com>
2011-03-06 19:59:46 -0800

    Changed layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,17 @@
 module ApplicationHelper
   
+  require 'uri'
+  
   def extended_username
     if current_user.admin?
       "#{current_user.username}(A)"
     else
       current_user.username
     end
+  end
+  
+  def host_only(url)
+    URI.parse(url).host
   end
 
 end
